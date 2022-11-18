f3e6924e792538b56ea6eea6566509a4d60acc15
KrzysztofBorejko <krzysiek.13.krzysiek@gmail.com>
2022-11-19 00:33:33 +0100

    Work with EventStoreRepository

---
+++ CQRS-ES/CQRS.Core/Domain/IEventStoreRepository.cs
@@ -0,0 +1,11 @@
+using System;
+using CQRS.Core.Events;
+
+namespace CQRS.Core.Domain
+{
+    public interface IEventStoreRepository
+    {
+        Task SaveAsync(EventModel @event);
+        Task<List<EventModel>> FindAggregatebyId(Guid aggregateId);
+    }
+}
